8c49650df192158ebb7a5ebd3d907c30c957513d
Kubeneo <kubeneo2009@gmail.com>
2017-06-26 17:49:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 	echo 'This is a php web app';
+	echo '<br>';
 	echo 'This is the second line';
+	echo '<br/>';
+
+	$result = 0;
+	for ( $i = 0; $i < 10; $i++ ){
+		$result += $i ;
+	}
+	echo 'the result is ' . $result;
 ?>
