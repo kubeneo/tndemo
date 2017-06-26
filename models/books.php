ff87ce0701d04ddb5170a1b4cd90e92346f65ccf
Kubeneo <kubeneo2009@gmail.com>
2017-06-26 18:20:18 +0800

    Update books.php

--- models/books.php
+++ models/books.php
@@ -5,7 +5,7 @@ class book{
 	public $price;
 }
 
-/*
+
 echo 'book<br/>';
 $b = new book;
 $b->book_id = '0001';
@@ -13,5 +13,5 @@ $b->book_name = 'Hello PHP';
 $b->price = 100;
 
 echo $b->book_id . ' ' . $b->book_name . ' ' . $b->price ;
-*/
+
 ?>
